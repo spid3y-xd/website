0fcd8f8608b4db03a0ca6b819a0d33a44222a6f8
spid3y-xd <binfarooq06@gmail.com>
2026-09-08 23:53:57 +0500

    Add files via upload

---
+++ UrbanFork/serve.ps1
@@ -0,0 +1,46 @@
+$port = 8080
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Start()
+Write-Host "Serving static files from $(Get-Location) on http://localhost:$port/"
+
+$mimeMap = @{
+    ".html" = "text/html; charset=utf-8"
+    ".css"  = "text/css; charset=utf-8"
+    ".js"   = "application/javascript; charset=utf-8"
+    ".jpg"  = "image/jpeg"
+    ".jpeg" = "image/jpeg"
+    ".png"  = "image/png"
+    ".svg"  = "image/svg+xml"
+    ".ico"  = "image/x-icon"
+}
+
+while ($listener.IsListening) {
+    $context = $listener.GetContext()
+    $request = $context.Request
+    $response = $context.Response
+
+    $urlPath = $request.Url.LocalPath.TrimStart('/')
+    if ([string]::IsNullOrWhiteSpace($urlPath)) {
+        $urlPath = "index.html"
+    }
+
+    $filePath = Join-Path (Get-Location) $urlPath
+
+    if (Test-Path $filePath -PathType Leaf) {
+        $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+        $contentType = if ($mimeMap.ContainsKey($ext)) { $mimeMap[$ext] } else { "application/octet-stream" }
+        $response.ContentType = $contentType
+        
+        $bytes = [System.IO.File]::ReadAllBytes($filePath)
+        $response.ContentLength64 = $bytes.Length
+        $response.OutputStream.Write($bytes, 0, $bytes.Length)
+        $response.StatusCode = 200
+    } else {
+        $response.StatusCode = 404
+        $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
+        $response.ContentLength64 = $msg.Length
+        $response.OutputStream.Write($msg, 0, $msg.Length)
+    }
+    $response.OutputStream.Close()
+}
